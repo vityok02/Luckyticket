264a045b2796888f2ba3e4c497cce3526343f1d9
Victor <lipkan02@gmail.com>
2022-04-03 14:30:22 +0300

    Длина строки

--- LuckyTicket/LuckyTicket/Program.cs
+++ LuckyTicket/LuckyTicket/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Привет, введите номер билета");
+string a = Console.ReadLine();
+Console.WriteLine($"Your ticket`s number is: {a}");
+if (a.Length < 4)
+{
+    Console.WriteLine("Твой билет имеет недостаточное количество символов");
+}
+else if (a.Length > 8)
+{
+    Console.WriteLine("Твой билет имеет много символов");
+}
